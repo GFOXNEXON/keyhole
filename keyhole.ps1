b53b0bb0240f82608ef628a720fb8c607e5cfc91
GFOXNEXON <102495576+GFOXNEXON@users.noreply.github.com>
2023-09-20 16:07:25 +1000

    Create keyhole.ps1

---
+++ keyhole.ps1
@@ -0,0 +1,17 @@
+#Load DLLs
+Add-Type -AssemblyName System.Windows.Forms
+
+# variable to sync between runspaces
+$sync = [Hashtable]::Synchronized(@{})
+$sync.PSScriptRoot = $PSScriptRoot
+$sync.version = "23.09.14"
+$sync.configs = @{}
+$sync.ProcessRunning = $false
+
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "Winutil needs to be run as Administrator. Attempting to relaunch."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "iwr -useb https://christitus.com/win | iex"
+    break
+}
+Function Get-WinUtilCheckBoxes {
